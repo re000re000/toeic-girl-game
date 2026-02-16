e73a90465b3b496789f7055b2c12fe99666274da
なのらぁ。 <96787540+re000re000@users.noreply.github.com>
2026-02-16 17:18:45 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '', // ここを空文字にします。これで「今いる場所」を基準にします
+  base: './', 
+  build: {
+    rollupOptions: {
+      output: {
+        // [重要] Date.now() を入れることで、毎回違う名前のファイルを作らせます
+        entryFileNames: `assets/[name]-${Date.now()}.js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`
+      }
+    }
+  }
 })
