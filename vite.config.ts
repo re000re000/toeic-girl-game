e845b8f311d7ab31eeb8a74c8077b1b5f7b58fc8
なのらぁ。 <96787540+re000re000@users.noreply.github.com>
2026-02-24 17:01:33 +0900

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    rollupOptions: {
-      output: {
-        // ファイル名に実行時の時間を刻印して、キャッシュを物理的に破壊します
-        entryFileNames: `assets/[name]-${Date.now()}.js`,
-        chunkFileNames: `assets/[name]-[hash].js`,
-        assetFileNames: `assets/[name]-[hash].[ext]`
-      }
-    }
-  }
+  base: '/toeic-girl-game/',
 })
